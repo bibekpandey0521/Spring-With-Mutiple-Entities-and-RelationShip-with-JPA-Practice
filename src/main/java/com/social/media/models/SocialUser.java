0d3bec8def970e0427b0ca9c5b08a08f24c5f49c
bibekpandey0521 <bibekpandey276@gmail.com>
2025-05-12 16:36:48 +0545

    Learned about many to many relationships

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.*;
 
@@ -15,12 +17,18 @@ public class SocialUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	
+
 	@OneToOne(mappedBy = "user")
 //	@JoinColumn(name = "social_profile_id")
 	private SocialProfile socialProfile;
-	
+
 	@OneToMany(mappedBy = "socialUser")
 	private List<Post> posts = new ArrayList<>();
+
+	@ManyToMany
+	@JoinTable(
+			name = "user_group", 
+			joinColumns = @JoinColumn(name = "user_id"), 
+			inverseJoinColumns = @JoinColumn(name = "group_id"))
+	private Set<SocialGroup> groups = new HashSet<>();
 }
